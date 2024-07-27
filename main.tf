984ee977cf94621e2d97e0910f19e972c0e12eed
Gaston Barbaccia <gastonbarbaccia@hotmail.com>
2024-07-27 01:20:56 -0300

    ok

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 resource "aws_instance" "instancia_ejemplo_01" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.small"
-  key_name      = "vockey"
   tags = {
     Name = "instancia_ejemplo_01"
   }
