67f41194bcd7926c96c9ef2e4dd1ae4345c351f8
Gaston Barbaccia <gastonbarbaccia@hotmail.com>
2024-07-27 02:25:35 -0300

    ok

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # Creamos una instancia EC2
-
+/*
 resource "aws_instance" "instancia_ejemplo_01" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.small"
   tags = {
-    Name = "instancia_ejemplo_02"
+    Name = "instancia_ejemplo_01"
   }
 }
+*/
